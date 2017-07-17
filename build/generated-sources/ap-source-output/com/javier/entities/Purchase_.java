f60f7a227138a576740fda1282d340f24f974682
javier-arz <javier-arz@hotmail.com>
2017-07-17 18:23:33 -0500

    avances subida imagenes

--- build/generated-sources/ap-source-output/com/javier/entities/Purchase_.java
+++ build/generated-sources/ap-source-output/com/javier/entities/Purchase_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-16T14:55:39")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-07-17T17:45:24")
 @StaticMetamodel(Purchase.class)
 public class Purchase_ { 
 
--- src/java/com/javier/managedbeans/ItemMB.java
+++ src/java/com/javier/managedbeans/ItemMB.java
@@ -7,11 +7,16 @@ package com.javier.managedbeans;
 
 import com.javier.ejb.ItemFacade;
 import com.javier.entities.Item;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.RequestScoped;
+import javax.servlet.http.Part;
 
 /**
  *
@@ -23,6 +28,11 @@ public class ItemMB {
 
     @EJB
     private ItemFacade itemFacade;
+
+    public static final String ITEM_UPLOAD_FOLDER = "images/items/";
+
+    // Element to receive the file images
+    private Part imageFile;
 
     private Item item;
 
@@ -47,6 +57,14 @@ public class ItemMB {
         this.item = item;
     }
 
+    public Part getImageFile() {
+        return imageFile;
+    }
+
+    public void setImageFile(Part file) {
+        this.imageFile = file;
+    }
+
     public List<Item> listItems() {
         return itemFacade.findAll();
     }
@@ -60,8 +78,39 @@ public class ItemMB {
     }
 
     public String saveItem() {
+        if (imageFile != null) {
+            saveImage();
+        }
+
         itemFacade.create(item);
         itemsList = listItems();
         return "welcomePrimefaces";
+
+    }
+
+    protected void saveImage() {
+        try {
+            InputStream in = imageFile.getInputStream();
+            String fileName = ITEM_UPLOAD_FOLDER + imageFile.getSubmittedFileName();
+            System.out.println("########################\n" + fileName + "\n########################\n");
+            File f = new File(fileName);
+            f.createNewFile();
+            FileOutputStream out = new FileOutputStream(f);
+            byte[] buffer = new byte[1024];
+
+            int length;
+
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+
+            item.setMainImageUrl(fileName);
+
+            out.close();
+            in.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
